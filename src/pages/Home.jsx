0840181941de3ae475777aebd09b69788b78cf38
User <emmanuellejupon@gmail.com>
2024-07-31 14:32:57 +0200

    add animation on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Mouse from "../components/Mouse";
 import SocialNetwork from "../components/SocialNetwork";
 import DynamicText from "../components/DynamicText";
 import Buttons from "../components/Buttons";
+import { motion } from "framer-motion";
 
 const Home = () => {
 	return (
@@ -14,13 +15,31 @@ const Home = () => {
 				<SocialNetwork />
 				<div className="home-main">
 					<div className="main-content">
-						<h1>FS AGENCY</h1>
-						<h2>
+						<motion.h1
+							drag
+							dragConstraints={{
+								left: -250,
+								right: 950,
+								top: -200,
+								bottom: 250,
+							}}
+						>
+							FS AGENCY
+						</motion.h1>
+						<motion.h2
+							drag
+							dragConstraints={{
+								left: -250,
+								right: 950,
+								top: -200,
+								bottom: 250,
+							}}
+						>
 							<DynamicText />
-						</h2>
+						</motion.h2>
 					</div>
 				</div>
-				<Buttons right={'/project-1'} />
+				<Buttons right={"/project-1"} />
 			</div>
 		</div>
 	);
